f29fdb359ecdfede0966cc02b42dacd538164e28
veyselguven <vgbasbaydar@gmail.com>
2023-12-17 12:14:50 -0800

    added some style and carlist component is done

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -1,7 +1,40 @@
 import React from "react";
-
+import { useSelector, useDispatch } from "react-redux";
+import { removeCar } from "../store";
 function CarList() {
-  return <div>CarList</div>;
+  const cars = useSelector((state) => {
+    return state.cars.data;
+  });
+  const dispatch = useDispatch();
+
+  const handleCarDelete = (car) => {
+    dispatch(removeCar(car.id));
+  };
+
+  const renderedCars = cars.map((car) => {
+    return (
+      <div key={car.id} className="panel">
+        <p>
+          {car.name} - ${car.cost}
+        </p>
+        <button
+          onClick={() => handleCarDelete(car)}
+          className="button is-danger"
+        >
+          Delete
+        </button>
+      </div>
+    );
+  });
+
+  console.log("cars=>", cars);
+
+  return (
+    <div className="car-list">
+      {renderedCars}
+      <hr />
+    </div>
+  );
 }
 
 export default CarList;
